8139641fbd255ce819fccb2e6b0e51a2bdc564f7
Murilo Goivino Tegani <murilo.tegani@ibm.com>
2023-05-04 16:54:06 -0300

    first commit

---
+++ src/main/java/br/com/heycheff/api/HeycheffApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.heycheff.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeycheffApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeycheffApiApplication.class, args);
+	}
+
+}
