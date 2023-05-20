e8f9ef99240f6dc7ea90fc32f33e4c0c43fa31fc
Murilo Goivino Tegani <murilo.tegani@ibm.com>
2023-05-20 19:49:56 -0300

    implementando MediaController

--- src/main/java/br/com/heycheff/api/service/FileService.java
+++ src/main/java/br/com/heycheff/api/service/FileService.java
@@ -1,10 +1,14 @@
 package br.com.heycheff.api.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,13 +22,30 @@ public class FileService {
 
     public String salvar(MultipartFile file, String fileName) {
         Path dirPath = Paths.get(basePath);
-        Path filePath = dirPath.resolve(fileName + "." +
-                Objects.requireNonNull(file.getContentType()).split("/")[1]);
+        String pathToSave = fileName + "." +
+                Objects.requireNonNull(file.getContentType()).split("/")[1];
+        Path filePath = dirPath.resolve(pathToSave);
+
         try {
             Files.createDirectories(dirPath);
             file.transferTo(filePath.toFile());
-            return filePath.toString();
+            return pathToSave;
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Resource getMedia(String path) {
+        Path dirPath = Paths.get(basePath);
+        Path file = dirPath.resolve(path);
+
+        try {
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable())
+                return resource;
+            else
+                throw new FileNotFoundException();
+        } catch (MalformedURLException | FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
